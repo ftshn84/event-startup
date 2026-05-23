8daee3d3990681aa63cd11974eb9b8a48abcd46b
Fatima <ftshn84@gmail.com>
2026-05-23 14:22:40 +0200

    feat: add root route to redirect to documentation

--- api/src/routers/index.js
+++ api/src/routers/index.js
@@ -3,6 +3,10 @@ import apiRouter from "#routers/api.js";
 
 const rootRouter = express.Router();
 
+rootRouter.get("/", (_req, res) => {
+    res.redirect("/docs");
+});
+
 rootRouter.use("/api", apiRouter);
 
 export default rootRouter;
